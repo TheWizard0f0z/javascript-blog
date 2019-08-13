c987f4fe3ff84a3ef5f20328500ab165842b2961
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-08-13 20:36:49 +0200

    Add js code
    get 'href' attribute from clicked link

--- js/script.js
+++ js/script.js
@@ -6,9 +6,10 @@
 });*/
 
 const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event)
+    console.log(event);
 
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
@@ -17,7 +18,7 @@ const titleClickHandler = function (event) {
         activeLink.classList.remove('active');
     }
 
-    /* [IN PROGRESS] add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
 
@@ -28,7 +29,9 @@ const titleClickHandler = function (event) {
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
     /* find the correct article using the selector (value of 'href' attribute) */
 
