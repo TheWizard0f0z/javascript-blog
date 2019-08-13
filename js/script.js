92e03a2709ba6985bfab2ab9c33848ff0e0388d4
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-08-13 20:45:07 +0200

    Add js code
    find correct article using selector

--- js/script.js
+++ js/script.js
@@ -33,7 +33,9 @@ const titleClickHandler = function (event) {
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
 
     /* add class 'active' to the correct article */
 }
