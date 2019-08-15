9c2a1c39404f8a9eb1a2bc1966edb406890c89ee
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-08-15 19:15:51 +0200

    Add js code
    split text into an array of words

--- js/script.js
+++ js/script.js
@@ -110,18 +110,25 @@ function generateTags() {
     console.log(articleTags);
 
     /* split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log(articleTagsArray);
 
     /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+      console.log(tag);
 
-    /* generate HTML of the link */
+      /* generate HTML of the link */
 
-    /* add generated code to html variable */
+      /* add generated code to html variable */
+    }
+
 
     /* END LOOP: for each tag */
 
     /* insert HTML of all the links into the tags wrapper */
 
     /* END LOOP: for every article: */
+
   }
 }
 
