b48e79203bc9ba4edcc9fd525ea797d2fbd1060f
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2019-08-18 17:30:03 +0200

    Add basic js code for tag cloud

--- js/script.js
+++ js/script.js
@@ -80,6 +80,9 @@ function generateTitleLinks(customSelector = '') {
 generateTitleLinks();
 
 function generateTags() {
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
+
   /* [DONE] find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
 
@@ -105,15 +108,27 @@ function generateTags() {
 
       /* [DONE] add generated code to html variable */
       html = html + linkHTML;
+
+      /* [NEW] check if this link is NOT already in allTags */
+      if (allTags.indexOf(linkHTML) == -1) {
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
     }
 
     /* END LOOP: for each tag */
 
     /* [DONE] insert HTML of all the links into the tags wrapper */
     tagsList.innerHTML = html;
-
-    /* END LOOP: for every article: */
   }
+
+  /* END LOOP: for every article: */
+
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector('.tags');
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' ');
 }
 
 generateTags();
